a6bf8e2491a0dd0edeed79a9a562a4b40eb2d6ef
macluiggy <luiggy.macias@imagineapps.co>
2024-04-22 12:17:59 -0500

    Add createCommentRouter to handle comments on posts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
-import { randomBytes } from "crypto";
+// import routes
 import createPostRouter from "./posts/routes/createPost";
+import createCommentRouter from "./comments/routes/createComment";
 const app = express();
 
 app.use(express.json());
 
+// posts
 app.use(createPostRouter);
+// comments
+app.use(createCommentRouter);
 
 app.listen(4000, () => {
   console.log("Listening on 4000");
