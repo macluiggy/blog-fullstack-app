63521bc1c0227e8a91ba7591f4643353ac246e81
macluiggy <luiggy.macias@imagineapps.co>
2024-04-22 11:07:44 -0500

    Add logging statement to backend/index.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { randomBytes } from "crypto";
+const app = express();
+
+app.use(express.json());
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+app.post("/posts", (req, res) => {
+  console.log("I am in");
+  const id = randomBytes(4).toString("hex");
+  const { title, content } = req.body;
+  posts[id] = {
+    id,
+    title,
+    content,
+  };
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
